80e43165a0e53431caa62e07d2eb7d6f2bd5f24c
Gloriya2001 <glooriaa2001@gmail.com>
2024-06-12 00:59:04 +0530

    add updation done

--- app.js
+++ app.js
@@ -3,15 +3,22 @@ const mongoose = require("mongoose")
 const cors = require("cors")
 const {playschoolmodel} = require("./models/playschool")
 
-
+mongoose.connect("mongodb+srv://gloria2001:gloria2001@cluster0.ipg35w1.mongodb.net/playschoolDB?retryWrites=true&w=majority&appName=Cluster0")
 
 const app = express()
 app.use(cors())
-app.use(express())
+app.use(express.json())
 
 
 app.post("/add",(req,res)=>{
-    res.send("success")
+
+    let input = req.body
+
+    const playschool = new playschoolmodel(input)
+    playschool.save()
+    console.log(playschool)
+
+    res.json({"status":"success"})
 })
 
 app.get("/view",(req,res)=>{
