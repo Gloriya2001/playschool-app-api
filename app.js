39151d28198903d1076edbda20fb1029f02ee9e1
Gloriya2001 <glooriaa2001@gmail.com>
2024-06-12 00:31:20 +0530

    Basic structure created

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+
+const app = express()
+app.use(cors())
+app.use(express())
+
+
+app.post("/add",(req,res)=>{
+    res.send("success")
+})
+
+app.get("/view",(req,res)=>{
+    res.send("view")
+})
+
+app.listen(8080,()=>{
+    console.log("Server Started")
+})
